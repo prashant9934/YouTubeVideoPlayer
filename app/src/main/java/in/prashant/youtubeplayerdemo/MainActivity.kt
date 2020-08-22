2e33102924936effacf81f9b005f0ea0c3a8e795
prashantkumar <prashant@market-pulse.in>
2020-08-22 17:30:59 +0530

    added readme details

--- app/src/main/java/in/prashant/youtubeplayerdemo/MainActivity.kt
+++ app/src/main/java/in/prashant/youtubeplayerdemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package `in`.prashant.youtubeplayerdemo
 
+import `in`.prashant.youtubeplayerdemo.databinding.ActivityMainBinding
 import `in`.prashant.youtubevideoplayer.YouTubeVideoPlayer
 import `in`.prashant.youtubevideoplayer.callBack.PlayerCallback
 import `in`.prashant.youtubevideoplayer.enums.PLAYER_STATE
@@ -12,19 +13,20 @@ import android.view.View
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var player: YouTubeVideoPlayer
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        player = findViewById(R.id.video_player)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 
     override fun onResume() {
         super.onResume()
         val videoParams = PlayerParams()
-        player.setAutoPlayerHeight()
+        binding.videoPlayer.setAutoPlayerHeight()
         videoParams.setControls(1)
         videoParams.setAutoplay(0)
         videoParams.setMute(0)
@@ -33,13 +35,13 @@ class MainActivity : AppCompatActivity() {
         videoParams.setDisablekb(0)
         videoParams.setEnablejsapi(1)
         videoParams.setFS(0)
-        player.initializeWithUrl(
+        binding.videoPlayer.initializeWithUrl(
             "https://www.youtube.com/watch?v=CQK7SHNcHqA",
             videoParams,
             getCallBack()
         )
 
-        player.isMuted {
+        binding.videoPlayer.isMuted {
             Log.d("ismuted", it.toString())
         }
     }
@@ -48,7 +50,7 @@ class MainActivity : AppCompatActivity() {
         return object : PlayerCallback {
             override fun onReady() {
                 Handler(Looper.getMainLooper()).post {
-                    player.play()
+                    binding.videoPlayer.play()
                     findViewById<ProgressBar>(R.id.progressBar).visibility = View.GONE
                 }
             }
